6c4c36fb3364bd16fa0ceb2711396d0fb55b5dcc
Pep1t0 <daniel.requena@aol.com>
2022-08-28 17:28:54 +0200

    v3

--- scanner-thread.py
+++ scanner-thread.py
@@ -15,41 +15,47 @@ def checkPort(target,port):
         if result == 0:
             print("Puerto {} abierto".format(port))
         s.close()
-            
-    except KeyboardInterrupt:
-        print('\n[!] Saliendo del programa')
-        sys.exit()
-
+                
     except socket.gaierror:
         print('\[!] No se puede hacer la resolucion de nombres')
-        
 
     except socket.error:
         print("\n[!] No se puede establecer conexión con el servidor")
 
 
 #Define nuestro objetivo - A MEJORAR CON ARGPARSER
-if len(sys.argv) == 2:
+if len(sys.argv) == 4:
     target = socket.gethostbyname(sys.argv[1]) #Conversion de hostname a IPv4
 else:
     print('Argumentos invalidos')
-    print('Sintaxis: python3 scanner.py <ip>')
+    print('Sintaxis: python3 scanner-thread.py <ip> <start_port> <end_port>')
+    sys.exit()
     
 #Banner - A MEJORAR
 print('-' * 50)
 print('Escaneando objetivo...' + target)
-start_time = time.time()
-print('Hora de inicio: ' + str(datetime.now()))
+print('Puertos:',sys.argv[2], sys.argv[3], sep='  ')
+print('Threads:',NUM_WORKERS)
+now = datetime.now()
+print('Hora de inicio: ' + now.strftime("%d/%m/%Y %H:%M:%S"))
 print('-' * 50)
+
+start_time = time.time()
        
-with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
-        futures = {executor.submit(checkPort, target, port) for port in range(1,1000)}
-        concurrent.futures.wait(futures)
+try:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
+            futures = {executor.submit(checkPort, target, port) for port in range(int(sys.argv[2]),int(sys.argv[3]))}
+            concurrent.futures.wait(futures)
+
+except KeyboardInterrupt:
+    print('[!] Control+C detectado. Finalizando threads...')   
         
 end_time = time.time()
+now = datetime.now()
 
-print('Hora de finalizacion: ' + str(datetime.now()))
-print('Tiempo transcurrido: %ssecs' %(end_time - start_time))    
-
+print('-' * 50)
+print('Hora de finalizacion: ' + now.strftime("%d/%m/%Y %H:%M:%S"))
+print('Tiempo transcurrido: %s segundos' %(end_time - start_time))    
+print('-' * 50)
     
     
